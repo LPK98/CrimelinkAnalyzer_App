14994548c82ba04c93bba86c555d5a5606a83294
Arosha Udaraka <aroshaudaraka99@gmail.com>
2026-01-26 18:43:23 +0530

    add leaveService file

---
+++ src/services/leaveService.ts
@@ -0,0 +1,17 @@
+// src/services/leaveService.ts
+import { api } from "../api/api";
+import type { LeaveRequest, LeaveSubmitRequest } from "../types/leave";
+
+export const getOfficerLeaves = async (
+  officerId: string,
+): Promise<LeaveRequest[]> => {
+  const res = await api.get(`/api/leaves/officer/${officerId}`);
+  return res.data;
+};
+
+export const submitLeaveRequest = async (
+  request: LeaveSubmitRequest,
+): Promise<LeaveRequest> => {
+  const res = await api.post(`/api/leaves/request`, request);
+  return res.data;
+};

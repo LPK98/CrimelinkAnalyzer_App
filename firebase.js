c2ac6272997d5cad22b9413663c48d1e198c9283
Lal Pushpakumaran <161225569+LPK98@users.noreply.github.com>
2026-02-08 17:54:25 +0530

    Update Firebase configuration to use environment variables and add chat functionality with image support

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUTM7T4PU0wZP2Qs5nWIGNM5D1wrWE0-o",
-  authDomain: "chatapp-3b437.firebaseapp.com",
-  projectId: "chatapp-3b437",
-  storageBucket: "chatapp-3b437.firebasestorage.app",
-  messagingSenderId: "822724763822",
-  appId: "1:822724763822:web:3ed2eb039b703b00dacd3f"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase only if no app exists
